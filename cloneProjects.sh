8bdc04e27f5a512b5f873a987c42ed3f2abc4372
Roger Dass <roger.dass@frustuminteractive.com>
2022-11-29 00:00:25 -0800

    CHG: helper script to clone often used projects into projects folder.

---
+++ cloneProjects.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd projects
+git clone git@github.com:FrustumInteractive/gfx.git
+git clone git@github.com:FrustumInteractive/math.git
+git clone git@github.com:FrustumInteractive/sandbox.git
+git clone git@github.com:FrustumInteractive/veggieJump.git
