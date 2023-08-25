1c9049c5006023135c7585b06aa133f2ce7ee926
Roger Dass <roger.dass@frustuminteractive.com>
2023-08-24 23:39:19 -0700

    CHG: added a new sting util and enclosing namspace.

--- include/fi/fiUtil.h
+++ include/fi/fiUtil.h
@@ -6,6 +6,29 @@
 
 #include "debug/trace.h"
 
+namespace FI {
+namespace UTIL {
+
+// incrementing filenames
+inline std::string incrementName(const std::string& name)
+{
+	size_t split = name.length();
+	while (split > 0)
+	{
+		if (!isdigit(name[split - 1]))
+			break;
+		split--;
+	}
+
+	if (split < name.length())
+	{
+		std::string prefix = name.substr(0, split);
+		std::string suffix = name.substr(split, name.length());
+		return prefix + std::to_string(std::stoi(suffix) + 1);
+	}
+	return name + "2";
+}
+
 #ifdef __ANDROID_API__
 
 namespace std {
@@ -22,5 +45,8 @@ FILE* android_fopen(const char* fname, const char* mode);
 #define fopen(name, mode) android_fopen(name, mode)
 
 #endif /*__ANDROID_API__*/
+
+} /*NS UTIL*/
+} /*NS FI*/
 
 #endif /*_FI_UTIL_*/
